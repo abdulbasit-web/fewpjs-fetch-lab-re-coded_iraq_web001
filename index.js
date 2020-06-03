d2f27c922beb38edebd3e348d07455ae60ec734f
Abdulbasit Salah <abdullbasit99salah@gmail.com>
2020-06-03 01:37:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-fetchBooks()
+function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      renderBooks(data);
+    });
+}
+
+// function renderBooks(json) {
+//   const main = document.querySelector("main");
+//   json.forEach((book) => {
+//     const h2 = document.createElement("h2");
+//     h2.innerHTML = `<h2>${book.name}</h2>`;
+//     main.appendChild(h2);
+//   });
+// }
+
+function renderBooks(data) {
+  const main = document.querySelector("main");
+  data.forEach((book) => {
+    const h2 = document.createElement("h2");
+    h2.innerHTML = `${book.name}`;
+    main.appendChild(h2);
+  });
+}
+
+fetchBooks();
+document.addEventListener("DOMContentLoaded", function () {});
